f0559b07ed264848862990d430d1c37d3e33228d
Walparis <walparis1@mail.ru>
2024-09-12 23:43:25 +0500

    signup post req

--- app/services/apiService.ts
+++ app/services/apiService.ts
@@ -20,6 +20,28 @@ const apiService = {
         });
     });
   },
+  post: async function (url: string, data: any): Promise<any> {
+    console.log("Post", url, data);
+
+    return new Promise((resolve, reject) => {
+      fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`, {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: data,
+      })
+        .then((response) => response.json())
+        .then((json) => {
+          console.log("Response", json);
+          resolve(json);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  },
 };
 
 export default apiService;
