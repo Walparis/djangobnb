b2d0dae31d9336e6efb74fa8c0e81be4239ffeb5
Walparis <walparis1@mail.ru>
2024-09-22 23:57:24 +0500

    booking and storing reservations to block incoming booking to already reserved property based on dates

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import Image from "next/image";
 
 import apiService from "@/app/services/apiService";
+import { getUserId } from "@/app/lib/actions";
 
 import ReservationSidebar from "@/app/components/properties/ReservationSidebar";
 
 const PropertyDetailPage = async ({ params }: { params: { id: string } }) => {
   const property = await apiService.get(`/api/properties/${params.id}`);
+
+  const userId = await getUserId();
 
   return (
     <main className="max-w-[1500px] mx-auto px-6 pb-6">
@@ -23,10 +26,11 @@ const PropertyDetailPage = async ({ params }: { params: { id: string } }) => {
         <div className="py-6 pr-6 col-span-3">
           <h1 className="mb-4 text-4xl">{property.title}</h1>
 
-          <span className="mb-6 block text-lg text-gray-600">
-            {property.guests} guest(s) - {property.bedrooms} bedroom(s) -{" "}
-            {property.bathrooms} bathroom(s)
-          </span>
+          <div className="mb-6 block text-lg text-gray-600">
+            <span>{property.guests} guest(s)</span> -{" "}
+            <span>{property.bedrooms} bedroom(s)</span> -{" "}
+            <span>{property.bathrooms} bathroom(s)</span>
+          </div>
 
           <hr />
 
@@ -51,7 +55,7 @@ const PropertyDetailPage = async ({ params }: { params: { id: string } }) => {
           <p className="mt-6 text-lg">{property.description}</p>
         </div>
 
-        <ReservationSidebar property={property} />
+        <ReservationSidebar property={property} userId={userId} />
       </div>
     </main>
   );
